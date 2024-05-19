14a5f7159ee4944f18834f89ce035db19e39109a
B SRINIVASA ASHRITH <srinivasaashrithb@gmail.com>
2024-05-20 01:34:45 +0530

    Add files via upload

---
+++ TASK 2 - TO-DO LIST/script.js
@@ -0,0 +1,79 @@
+let taskCount = 0;
+const addBtn = document.querySelector("#add-btn");
+const newTaskInput = document.querySelector("#wrapper input");
+const tasksContainer = document.querySelector("#tasks");
+const error = document.getElementById("error");
+const countValue = document.querySelector(".count-value");
+
+window.onload=()=>{
+  taskCount=0;
+  displayCount(taskCount);
+  newTaskInput.value="";
+};
+
+const displayCount = (taskCount) => {
+  countValue.innerText = taskCount;
+};
+
+const addTask = () => {
+  const taskName = newTaskInput.value.trim();
+  error.style.display = "none";
+  if (!taskName) {
+    setTimeout(() => {
+      error.style.display = "block";
+    }, 200);
+    return;
+  }
+  const task = `<div class="task">
+                    <input type="checkbox" class="task-check">
+                    <span class="taskname">${taskName}</span>
+                    <button class="edit">
+                      <i class="fa-solid fa-pen-to-square" style="color: #000000;"></i>
+                    </button>
+                    <button class="delete">
+                      <i class="fa-solid fa-trash" style="color: #000000;"></i>
+                    </button>
+                  </div>`;
+
+  tasksContainer.insertAdjacentHTML("beforeend", task);
+
+  const deleteButtons = document.querySelectorAll(".delete");
+  deleteButtons.forEach((button) => {
+    button.onclick = () => {
+      button.parentNode.remove();
+      taskCount -= 1;
+      displayCount(taskCount);
+    };
+  });
+  const editButtons = document.querySelectorAll(".edit");
+  editButtons.forEach((editBtn) => {
+    editBtn.onclick = (e) => {
+      let targetElement = e.target;
+      if (!(e.target.className == "edit")) {
+        targetElement = e.target.parentElement;
+      }
+      newTaskInput.value = targetElement.previousElementSibling.innerText;
+      targetElement.parentNode.remove();
+      taskCount -= 1;
+      displayCount(taskCount);
+    };
+  });
+  const tasksCheck = document.querySelectorAll(".task-check");
+  tasksCheck.forEach((checkBox) => {
+    checkBox.onchange = () => {
+      checkBox.nextElementSibling.classList.toggle("completed");
+      if (checkBox.checked) {
+        taskCount = taskCount;
+      } 
+      else {
+        taskCount += 1;
+      }
+      displayCount(taskCount);
+    };
+  });
+  taskCount += 1;
+  displayCount(taskCount);
+  newTaskInput.value = "";
+};
+
+addBtn.addEventListener("click", addTask);
